4c8a1710b402be137c2987f1dbe972e1250ff349
NoKi <novak.kirill@gmail.com>
2021-09-08 18:43:18 +0300

    add count-down timer

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { View, Text, Animated } from 'react-native'
 import { useTheme } from '@/Theme'
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
+import Cards from '@/Containers/Cards'
 
 interface CardProps {
   id: number
@@ -20,19 +21,28 @@ const Card = () => {
         // { backgroundColor: 'green' },
       ]}
     >
-      <View style={[Layout.fill, Layout.fullWidth]}>
-        <Text style={[Gutters.largeVMargin]}>Imege</Text>
-        {/* <CountdownCircleTimer
-          isPlaying
-          duration={10}
-          colors={[['#004777', 0.33], ['#F7B801', 0.33], ['#A30000']]}
-        >
-          {({ remainingTime, animatedColor }) => (
-            <Animated.Text style={{ color: animatedColor }}>
-              {remainingTime}
-            </Animated.Text>
-          )}
-        </CountdownCircleTimer> */}
+      <View style={[Layout.fill, Layout.fullWidth, Layout.row]}>
+        <View style={[Gutters.largeVMargin, Layout.fill]}>
+          <Text
+            // style={[Gutters.largeVMargin]}
+          >
+            Imege
+          </Text>
+        </View>
+        <View style={[Gutters.largeVMargin, Layout.fill]}>
+          <CountdownCircleTimer
+            isPlaying
+            size={110}
+            duration={10}
+            colors={[['#4EC5F1', 0.33], ['#F7B801', 0.33], ['#A30000']]}
+          >
+            {({ remainingTime, animatedColor }) => (
+              <Animated.Text style={{ color: animatedColor }}>
+                {remainingTime}
+              </Animated.Text>
+            )}
+          </CountdownCircleTimer>
+        </View>
       </View>
       <View style={[Layout.fill, Layout.fullWidth, Layout.colHCenter]}>
         <Text style={[Fonts.textLeft, Layout.fillWidth]}>TITLE</Text>
